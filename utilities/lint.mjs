7aeb92002c26264825222ce1aa25e0f5892ab649
Ben Pardo <visaudi@gmail.com>
2018-05-03 15:24:42 -0600

    Scripts linting of all JavaScript files.

--- utilities/lint.mjs
+++ utilities/lint.mjs
@@ -3,17 +3,19 @@
 */
 import jslint from "../dependencies/jslint";
 import fs from "fs";
-const fileName = process.argv[2];
+const fileNames = process.argv.slice(2);
 
-fs.readFile(fileName, "utf-8", function (error, fileText) {
-    if (error) {
-        throw error;
-    }
-    const lintedCode = jslint(fileText);
-    if (lintedCode.ok === true) {
-        console.log(fileName + " is okay.");
-    }
-    if (lintedCode.ok === false) {
-        console.log(lintedCode.warnings);
-    }
+fileNames.forEach(function (fileName) {
+    fs.readFile(fileName, "utf-8", function (error, fileText) {
+        if (error) {
+            throw error;
+        }
+        const lintedCode = jslint(fileText);
+        if (lintedCode.ok === true) {
+            console.log(fileName + " is okay.");
+        }
+        if (lintedCode.ok === false) {
+            console.log(lintedCode.warnings);
+        }
+    });
 });
